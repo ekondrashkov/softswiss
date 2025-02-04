15f99a5e902e218598c207fee77547d255053cf8
Evgenii Kondrashkov <kondrashkov@mail.ru>
2025-02-04 13:15:04 +0100

    Footer: tag name change
    Change tag name to footer in Footer component

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -4,11 +4,11 @@ import rocket from "../../../assets/images/rocket.png"
 
 export const Footer = () => {
   return (
-    <header className="footer">
+    <footer className="footer">
       <Container className="footer__container">
         <img className="footer__img" src={rocket} alt="Rocket" />
         <span className="footer__text">Exciting space adventure!</span>
       </Container>
-    </header>
+    </footer>
   )
 }
